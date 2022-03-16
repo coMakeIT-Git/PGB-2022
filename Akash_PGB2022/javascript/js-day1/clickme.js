a124ebf0a3937e6c04944f94a47d9d7556831576
Akash Nerella <100755887+akashnerella@users.noreply.github.com>
2022-03-16 11:29:10 +0530

    js day1 committed

--- Akash_PGB2022/javascript/js-day1/clickme.js
+++ Akash_PGB2022/javascript/js-day1/clickme.js
@@ -1,12 +1,13 @@
-let counterE = document.getElementById("counterValue");
-let clickCount = localStorage.getItem("clickCount");
-if (clickCount === null) {
-    counterE.textContent = 0;
-} else {
-    counterE.textContent = clickCount;
-}
+
 
 function onIncrementCount() {
+    let counterE = document.getElementById("counterValue");
+    let clickCount = localStorage.getItem("clickCount");
+    if (clickCount === null) {
+        counterE.textContent = 0;
+    } else {
+        counterE.textContent = clickCount;
+    }
     let c = counterE.textContent;
     let d = parseInt(c) + 1;
     localStorage.setItem("clickCount", d);
