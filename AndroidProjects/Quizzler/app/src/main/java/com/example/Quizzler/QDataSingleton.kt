c918e8b72a4150c100576d7bf4f0c1932e5f3efb
Praphullag1002 <100762699+Praphullag1002@users.noreply.github.com>
2022-03-29 18:46:22 +0530

    Android Studio Quizzler commit

---
+++ AndroidProjects/Quizzler/app/src/main/java/com/example/Quizzler/QDataSingleton.kt
@@ -0,0 +1,19 @@
+package com.example.Quizzler
+
+import com.example.quizzler.QList
+
+
+// static singleton data source
+
+
+ class QDataSingleton {
+
+     companion object {
+         lateinit var QData : QList
+        var  score  = 0
+
+     }
+
+
+
+}
